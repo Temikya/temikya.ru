a7cadb0a1d04334ae1a3d772474e4afeaf3be8a4
Artyom Kulibaba <kulibaba948@gmail.com>
2026-06-13 21:27:44 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,140 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
+
+const movieRoutes =
+    require("./routes/movies");
+
+const aiRoutes =
+    require("./routes/ai");
+
+const app = express();
+
+
+// ======================
+// MIDDLEWARE
+// ======================
+
+app.use(cors({
+    origin: [
+        "https://temikya.ru",
+        "https://www.temikya.ru"
+    ]
+}));
+
+app.use(express.json());
+
+
+// ======================
+// LOGGER
+// ======================
+
+app.use(
+    morgan(
+        ":method :url :status :response-time ms"
+    )
+);
+
+
+// ======================
+// REQUEST LOGGER
+// ======================
+
+app.use((req, res, next) => {
+
+    console.log(
+        `📩 ${req.method} ${req.url}`
+    );
+
+    next();
+});
+
+
+// ======================
+// API ROUTES
+// ======================
+
+app.use(
+    "/api/movies",
+    movieRoutes
+);
+
+app.use(
+    "/api/ai",
+    aiRoutes
+);
+
+
+// ======================
+// STATIC FRONTEND
+// ======================
+
+app.use(
+    express.static(
+        path.join(
+            __dirname,
+            "public"
+        )
+    )
+);
+
+
+// ======================
+// MAIN PAGE
+// ======================
+
+app.get("/", (req, res) => {
+
+    res.sendFile(
+        path.join(
+            __dirname,
+            "public",
+            "index.html"
+        )
+    );
+});
+
+
+// ======================
+// GLOBAL ERROR HANDLER
+// ======================
+
+app.use(
+    (
+        err,
+        req,
+        res,
+        next
+    ) => {
+
+        console.error(
+            "❌ SERVER ERROR:"
+        );
+
+        console.error(err);
+
+        res.status(500).json({
+            error:
+                "Ошибка сервера"
+        });
+    }
+);
+
+
+// ======================
+// SERVER START
+// ======================
+
+const PORT =
+    process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+    console.log(`
+🚀 Server running
+🌍 http://localhost:${PORT}
+    `);
+});
